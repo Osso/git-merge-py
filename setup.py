7f26c48142774b8fa89c828a729d540d96d2f405
Alessio Deiana <adeiana@gmail.com>
2020-11-03 01:42:00 -0600

    Updates dependencies

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import (find_packages,
                         setup)
 
 setup(name="gitmergepy", packages=find_packages(),
-      install_requires=['redbaron'],)
+      dependency_links=['http://github.com/Osso/redbaron/tarball/master#egg=redbaron-1.0'])

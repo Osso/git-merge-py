18f4ef0d4b2d03cd170b3599627bdc3d3bdf1d64
Alessio Deiana <adeiana@gmail.com>
2020-11-14 18:12:36 -0600

    Try different name for redbaron

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import (find_packages,
                         setup)
 
 setup(name="gitmergepy", packages=find_packages(),
-      install_requires=["redbaron==1.0"],
-      dependency_links=['http://github.com/Osso/redbaron/tarball/master#egg=redbaron-1.0'])
+      install_requires=["redbaron2==1.0"],
+      dependency_links=['http://github.com/Osso/redbaron/tarball/master#egg=redbaron2-1.0'])
